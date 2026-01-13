b606ad80f32c83ec68b62d5ef22001c1fec37799
KonstantineBiganashvili <konstantine@biganashvili.dev>
2026-01-14 03:47:37 +0400

    Feat: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 import { AppModule } from './app.module';
-import { Logger } from 'winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -19,6 +20,15 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1');
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Currency Converter API')
+    .setDescription('API for converting currencies using Monobank exchange rates')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -29,6 +39,7 @@ async function bootstrap() {
 
   await app.listen(port);
   logger.log(`Application running on port ${port}`, 'Bootstrap');
+  logger.log(`Swagger docs available at http://localhost:${port}/api/v1/docs`, 'Bootstrap');
 }
 
 void bootstrap();
